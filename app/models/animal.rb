af7aacfddf0131365a880d50da36b31de7f625a8
Qu4k3 <paul.guillamon@gmail.com>
2019-06-01 17:11:18 +0200

    show animal photo correctly

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,13 +2,13 @@ class Animal < ApplicationRecord
   has_many :users
   belongs_to :zone
 
-  #validates :name
-  #validates :breed
+  validates :name, presence: true
+  validates :breed, presence: true
   validates :year_birth, numericality: { only_integer: true }
 
   mount_uploader :animal_photo, AnimalPhotoUploader
   # Animal photo validation
   validates_integrity_of :animal_photo
   validates_processing_of :animal_photo
-  validates :animal_photo, file_size: { less_than: 1.megabytes }
+  validates :animal_photo, file_size: { less_than: 5.megabytes }
 end
